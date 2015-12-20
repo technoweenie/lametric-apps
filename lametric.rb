cc4d98f50cfbffaf08762b7e306c25ffcb5dfef6
risk danger olson <technoweenie@gmail.com>
2015-12-20 14:11:41 -0700

    remember how sinatra splats work

--- lametric.rb
+++ lametric.rb
@@ -2,22 +2,9 @@ require "sinatra"
 require "json"
 require "octokit"
 
-get "/git-lfs" do
-  cache_control :public, max_age: 540
-
-  repository = Octokit.repository("github/git-lfs")
-  {
-    :frames => [
-      :index => 0,
-      :text => "Git LFS: #{repository.stargazers_count}/#{repository.subscribers_count}",
-      :icon => "i2184",
-    ]
-  }.to_json
-end
-
 get "/github-repository-stats/*" do
   repos = []
-  params[:splat].each_with_index do |part, idx|
+  params[:splat].first.split("/").each_with_index do |part, idx|
     if idx % 2 == 0 # even
       repos << part
     else
@@ -27,6 +14,7 @@ get "/github-repository-stats/*" do
   repos.delete_if { |r| !r.include?("/") }
 
   cache_control :public, max_age: 540
+  content_type :json
 
   {
     :frames => [
