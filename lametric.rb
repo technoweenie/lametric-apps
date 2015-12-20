615b917772a8fb55c310b331c992b97bc754d717
risk danger olson <technoweenie@gmail.com>
2015-12-20 13:43:09 -0700

    lean into the ticker output

--- lametric.rb
+++ lametric.rb
@@ -3,11 +3,13 @@ require "json"
 require "octokit"
 
 get "/git-lfs" do
+  cache_control :public, max_age: 240
+
   repository = Octokit.repository("github/git-lfs")
   {
     :frames => [
       :index => 0,
-      :text => "#{repository.stargazers_count}",
+      :text => "Git LFS: #{repository.stargazers_count}/#{repository.watchers_count}",
       :icon => "i2184",
     ]
   }.to_json
